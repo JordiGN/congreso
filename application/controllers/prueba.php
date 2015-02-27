9b860309a3a3c8a35b1a455dcfbdaf58d80298d7
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-02-27 09:54:53 -0600

    26/02/15 creacion del proyecto
    Se creo el proyecto tomando en cuenta lo que se tenia ya anteriormente
    en la practica, se hara un proyecto con base de datos, con inicios,
    sesiones y documentacion para congresos, conferencias, y talleres

---
+++ application/controllers/prueba.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Prueba extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		#carga de librerias
+		#carga de helpers
+	}
+
+	public function index()
+	{
+		$datos['nombre']="Juan Perez";
+
+		$this->load->view('inicio',$datos);
+	}
+
+	public function opcion1()
+	{
+		
+		$this->load->view("vista1");
+	}
+
+	public function opcion2()
+	{
+		
+		$this->load->view('vista2');
+	}
+
+	public function opcion3()
+	{
+		
+		$this->load->view("vista3");
+	}
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
